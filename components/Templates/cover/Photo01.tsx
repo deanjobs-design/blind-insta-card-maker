6d17ea00ea7ee4fac3f8017b79bd9e5084a63fe3
deanjobs-design <je.jang@teamblind.com>
2026-07-14 10:40:57 +0900

    tweak: tighter headline line-height (1.3) on Photo01

--- components/Templates/cover/Photo01.tsx
+++ components/Templates/cover/Photo01.tsx
@@ -57,7 +57,7 @@ export function Photo01({ values }: Props) {
           fontFamily: "'Libre Baskerville', serif",
           fontWeight: 700,
           fontSize: 96,
-          lineHeight: 1.4,
+          lineHeight: 1.3,
           color: 'white',
           margin: 0,
           wordBreak: 'break-word',
